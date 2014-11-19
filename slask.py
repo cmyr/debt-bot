76069f657e260cd73c9b1f2504b5453831235226
cmyr <colin@cmyr.net>
2014-11-19 14:28:00 -0500

    cleanup & prelim slack api integration

--- slask.py
+++ slask.py
@@ -13,40 +13,9 @@ curdir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(curdir)
 
 from config import config
+from token import slack_token
 
-# hooks = {}
-# def init_plugins():
-#     for plugin in glob('plugins/[!_]*.py'):
-#         print "plugin: %s" % plugin
-#         try:
-#             mod = importlib.import_module(plugin.replace(os.path.sep, ".")[:-3])
-#             modname = mod.__name__.split('.')[1]
-#             for hook in re.findall("on_(\w+)", " ".join(dir(mod))):
-#                 hookfun = getattr(mod, "on_" + hook)
-#                 print "attaching %s.%s to %s" % (modname, hookfun, hook)
-#                 hooks.setdefault(hook, []).append(hookfun)
-
-#             if mod.__doc__:
-#                 firstline = mod.__doc__.split('\n')[0]
-#                 hooks.setdefault('help', {})[modname] = firstline
-#                 hooks.setdefault('extendedhelp', {})[modname] = mod.__doc__
-
-#         #bare except, because the modules could raise any number of errors
-#         #on import, and we want them not to kill our server
-#         except:
-#             print "import failed on module %s, module not loaded" % plugin
-#             print "%s" % sys.exc_info()[0]
-#             print "%s" % traceback.format_exc()
-
-# init_plugins()
-
-# def run_hook(hook, data, server):
-#     responses = []
-#     for hook in hooks.get(hook, []):
-#         h = hook(data, server)
-#         if h: responses.append(h)
-
-#     return responses
+slack = Slack(token)
 
 @app.route("/", methods=['POST'])
 def main():
@@ -58,8 +27,6 @@ def main():
     if username == msguser or msguser.lower() == "slackbot":
         return ""
 
-    # text = "\n".join(run_hook("message", request.form, {"config": config, "hooks": hooks}))
-    # if not text: return ""
     text = "hi colin!"
     response = {
         "text": text,
@@ -70,4 +37,4 @@ def main():
     return json.dumps(response)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
