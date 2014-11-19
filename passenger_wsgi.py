f907e1c1ddfca4bf309fb302ee3d3b7c609a5b06
cmyr <colin@cmyr.net>
2014-11-18 20:03:47 -0500

    changed passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -3,7 +3,7 @@ INTERP = os.path.join(os.environ['HOME'], 'flask_env', 'bin', 'python')
 if sys.executable != INTERP:
     os.execl(INTERP, INTERP, *sys.argv)
 sys.path.append(os.getcwd())
-from debtbot import app as application
+from debtbot.slask import app as application
 
 # Uncomment next two lines to enable debugging
 # from werkzeug.debug import DebuggedApplication
