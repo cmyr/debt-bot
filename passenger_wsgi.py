235891e950265afdbb63983c09eb8515710bcdbe
cmyr <colin@cmyr.net>
2014-11-18 20:11:45 -0500

    copying naqeeb's old passenger_wsgi.py =)

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,8 +1,9 @@
 import sys, os
-INTERP = os.path.join(os.environ['HOME'], 'flask_env', 'bin', 'python')
+INTERP = os.path.join(os.environ['HOME'], 'http://5l4ck.cmyr.net', 'flask_env', 'bin', 'python')
 if sys.executable != INTERP:
     os.execl(INTERP, INTERP, *sys.argv)
 sys.path.append(os.getcwd())
+sys.path.append('debtbot')
 from debtbot.slask import app as application
 
 # Uncomment next two lines to enable debugging
