d408b9616289603e5c1185f79971ae83ee613f11
cmyr <colin@cmyr.net>
2014-12-12 16:07:28 -0500

    refactored transaction stuff to be a bit more functional

--- transaction.py
+++ transaction.py
@@ -2,52 +2,70 @@
 from __future__ import print_function
 from __future__ import unicode_literals
 import re
+from collections import namedtuple
 
 # these are operator types. This is annoying. I'd love an enum
 FROM_KEY = "from"
 TO_KEY = "to"
 
+RawTransaction = namedtuple("Transaction", ["first_party", "operator", "second_party", "value", "notes"])
+
 class Transaction(object):
     """A transaction represents a debt relationship between two parties"""
-    debtor = ""
-    creditor = ""
+    debtor = None
+    creditor = None
     value = None
-    currency = "$"
-    notes = ""
+    currency = None
+    notes = None
 
-    _first_party = None
-    _second_party = None
-    _operator = None
-    _value = None
-    _notes = None
+    raw_transaction = None
 
     def __init__(self, input_str):
         super(Transaction, self).__init__()
-        self.parse_transaction(input_str)
+        raw_transaction = self.componentize_transaction(
+            input_str, 
+            [self.componentize1, self.componentize2]
+            )
+        self.parse_components(raw_transaction)
 
     def __repr__(self):
         return "%s %s %s %s %s" % (
-            self._first_party, self._second_party, self._operator,
-            self._value, self._notes)
+            self.debtor, self.creditor, self.value,
+            self.currency, self.notes)
 
-    def parse_transaction(self, text):
+    def componentize_transaction(self, text, funcs):
+        for f in funcs:
+            result = f(text)
+            if result:
+                return result
+
+    def componentize1(self, text):
         parsed = re.search(r'<?@(\w+)>?\s*(\S*)\s*<?@(\w+)>?\s.?\$?([0-9\.]+)\$?(.*)', text)
         if parsed:
-            self._first_party = parsed.group(1)
-            self._second_party = parsed.group(3)
-            self._operator = parsed.group(2)
-            self._value = parsed.group(4)
-            self._notes = parsed.group(5)
-            return
+            return RawTransaction(parsed.group(1), parsed.group(2), parsed.group(3),
+             parsed.group(4), parsed.group(5))
 
+    def componentize2(self, text):
         parsed = re.search(r'<?@(\w+)>?\s*(\S*)\s*<?@(\w+)>?\s(.*?)([0-9\.]+)', text)
         if parsed:
-            self._first_party = parsed.group(1)
-            self._second_party = parsed.group(3)
-            self._operator = parsed.group(2)
-            self._value = parsed.group(4)
-            self._notes = parsed.group(5)
-            return
+            return RawTransaction(parsed.group(1), parsed.group(2), parsed.group(3),
+             parsed.group(5), parsed.group(4))        
+
+
+    def parse_components(self, raw_transaction):
+        if raw_transaction:
+            operator = self.parse_operator(raw_transaction.operator)
+            if operator == TO_KEY:
+                self.debtor = raw_transaction.first_party
+                self.creditor = raw_transaction.second_party
+            elif operator == FROM_KEY:
+                self.debtor = raw_transaction.second_party
+                self.creditor = RawTransaction.first_party
+
+            self.operator = raw_transaction.operator
+            self.value = raw_transaction.value
+            self.notes = raw_transaction.notes
+
 
     def parse_operator(self, operator_text):
         if operator_text == "-&gt;":
