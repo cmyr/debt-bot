8de0b591253294472c19ff1cc253a19921cd9292
cmyr <colin@cmyr.net>
2014-12-15 12:07:58 -0500

    added some more test cases

--- tests.py
+++ tests.py
@@ -26,7 +26,13 @@ test_cases = [
     ('<@U024H5KK7> -&gt; <@U024H5LFB> 20$',
         TestExpectation('U024H5KK7', 'U024H5LFB', 20, None, None)),
     ('<@U024H4SR1>: -&gt; <@U024H5LFB> $7', 
-        TestExpectation('U024H4SR1', 'U024H5LFB', 7, None, None))
+        TestExpectation('U024H4SR1', 'U024H5LFB', 7, None, None)),
+    ('<@U02BMUNJ7> -&gt; <@U024H4SR1> 8ish pho',
+        TestExpectation('U02BMUNJ7', 'U024H4SR1', 8, None, 'ish pho')),
+    ('<@U02BMUNJ7>  -&gt; <@U02G8SVCB> $15',
+        TestExpectation('U02BMUNJ7', 'U02G8SVCB', 15, None, None)),
+    ('<@U02BMUNJ7> -&gt; <@U024H5LFB> : white wine : 9.75',
+        TestExpectation('U02BMUNJ7', 'U024H5LFB', 9.75, None, ' : white wine : '))
     ]
 
 
