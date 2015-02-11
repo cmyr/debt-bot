ed741e75f8bcfca7d943f9fed0b11e24e0ce5c7f
cmyr <colin@cmyr.net>
2015-02-11 17:28:48 -0500

    fixed some parsing issues

--- transaction.py
+++ transaction.py
@@ -10,9 +10,13 @@ DEBUG = False
 FROM_KEY = "from"
 TO_KEY = "to"
 
-RawTransaction = namedtuple("Transaction", ["first_party", "operator", "second_party", "value", "notes"])
+RawTransaction = namedtuple(
+    "Transaction",
+    ["first_party", "operator", "second_party", "value", "notes"])
+
 
 class Transaction(object):
+
     """A transaction represents a debt relationship between two parties"""
     debtor = None
     creditor = None
@@ -26,14 +30,14 @@ class Transaction(object):
     @property
     def parsed(self):
         return True if self.raw_transaction else False
-    
+
     def __init__(self, input_str):
         super(Transaction, self).__init__()
         self.raw_text = input_str
         self.raw_transaction = self.componentize_transaction(
-            input_str, 
+            input_str,
             [self.componentize1, self.componentize2]
-            )
+        )
         self.parse_components(self.raw_transaction)
 
     def __repr__(self):
@@ -51,25 +55,29 @@ class Transaction(object):
         return 0, None
 
     def componentize_transaction(self, text, funcs):
+        subbed = re.sub(r'<@(\w+)>', r'@\1 ', text)
         if DEBUG:
             print(text)
+            print(subbed)
         for f in funcs:
-            result = f(text)
+            result = f(subbed)
             if result:
                 return result
 
     def componentize1(self, text):
-        parsed = re.search(r'<?@(\w+)>?.?\s*(\S*)\s*<?@(\w+)>?\s?\$?([0-9\.]+)\$?(.*)', text)
+        parsed = re.search(
+            r'@(\w+).?\s*(\S*)\s*@(\w+)\s+\$?([0-9\.]+)\$?(.*)', text)
         if parsed:
-            return RawTransaction(parsed.group(1), parsed.group(2), parsed.group(3),
-             parsed.group(4), parsed.group(5))
+            return RawTransaction(
+                parsed.group(1), parsed.group(2), parsed.group(3),
+                parsed.group(4), parsed.group(5))
 
     def componentize2(self, text):
-        parsed = re.search(r'<?@(\w+)>?.?\s*(\S*)\s*<?@(\w+)>?\s(.*?)([0-9\.]+)', text)
+        parsed = re.search(r'@(\w+).?\s*(\S*)\s*(\w+)\s(.*?)([0-9\.]+)', text)
         if parsed:
-            return RawTransaction(parsed.group(1), parsed.group(2), parsed.group(3),
-             parsed.group(5), parsed.group(4))
-
+            return RawTransaction(
+                parsed.group(1), parsed.group(2), parsed.group(3),
+                parsed.group(5), parsed.group(4))
 
     def parse_components(self, raw_transaction):
         if raw_transaction:
@@ -81,7 +89,7 @@ class Transaction(object):
                 self.creditor = raw_transaction.second_party
             elif operator == FROM_KEY:
                 self.debtor = raw_transaction.second_party
-                self.creditor = RawTransaction.first_party
+                self.creditor = raw_transaction.first_party
 
             self.operator = raw_transaction.operator
             self.value = float(raw_transaction.value)
@@ -94,15 +102,18 @@ class Transaction(object):
         if operator_text == "-&lt;":
             return FROM_KEY
 
+
 def test():
- tests = [
-    '<@U024H4SR1> -&gt; <@U024H5LFB> $15 (thai food)',
-    '<@U024H5KK7> -&gt; <@U024H5LFB> 20$',
-    '<@U024H5KK7> -&gt; <@U024H5LFB> $10 sandwich place',
-    u'<@U024H4SR1|will> set the channel topic: trying to incur more debt to make the debt-bot more exciting',
- ]
- for t in tests:
-    print(Transaction(t).obligation_to_user('U024H5LFB'))
+    tests = [
+        '<@U024H4SR1> -&gt; <@U024H5LFB> $15 (thai food)',
+        '<@U024H5KK7> -&gt; <@U024H5LFB> 20$',
+        '<@U024H5KK7> -&gt; <@U024H5LFB> $10 sandwich place',
+        '<@U024H5KK7> -&lt; <@U024H5LFB> $10 sandwich place',
+        '<@U024H4SR1|will> set the channel topic: trying to incur \
+        more debt to make the debt-bot more exciting',
+    ]
+    for t in tests:
+        print(Transaction(t).obligation_to_user('U024H5LFB'))
 
 
 if __name__ == '__main__':
