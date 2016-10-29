36bd7d46dbbfb45d3cf8a68d80715fe353da5762
Colin Rofls <colin@cmyr.net>
2016-10-28 21:28:47 -0400

    restructure for incoming tests

--- tests.py
+++ tests.py
@@ -4,13 +4,15 @@ from __future__ import unicode_literals
 
 from transaction import *
 from collections import namedtuple
+import re
 
-TestExpectation = namedtuple("TransactionTestResult", 
+TestExpectation = namedtuple(
+    "TransactionTestResult",
     ["debtor", "creditor", "value", "currency", "notes"])
 
-def run_test(testcase):
-    result = Transaction(testcase[0])
-    expectation = testcase[1]
+
+def run_test(item, expectation):
+    result = Transaction(item)
 
     assert result.debtor == expectation.debtor, "incorrect debtor"
     assert result.creditor == expectation.creditor, "incorrect creditor"
@@ -21,29 +23,38 @@ def run_test(testcase):
 # :minuum: -&gt; <@U024H5LFB> dinner
 test_cases = [
     ('<@U024H4SR1> -&gt; <@U024H5LFB> $15 (thai food)',
-    TestExpectation('U024H4SR1', 'U024H5LFB', 15, None, ' (thai food)')
-        ),
+        TestExpectation('U024H4SR1', 'U024H5LFB', 15, None, ' (thai food)')),
     ('<@U024H5KK7> -&gt; <@U024H5LFB> 20$',
         TestExpectation('U024H5KK7', 'U024H5LFB', 20, None, None)),
-    ('<@U024H4SR1>: -&gt; <@U024H5LFB> $7', 
+    ('<@U024H4SR1> -&gt; <@U024H5LFB> $7',
         TestExpectation('U024H4SR1', 'U024H5LFB', 7, None, None)),
     ('<@U02BMUNJ7> -&gt; <@U024H4SR1> 8ish pho',
         TestExpectation('U02BMUNJ7', 'U024H4SR1', 8, None, 'ish pho')),
     ('<@U02BMUNJ7>  -&gt; <@U02G8SVCB> $15',
         TestExpectation('U02BMUNJ7', 'U02G8SVCB', 15, None, None)),
-    ('<@U02BMUNJ7> -&gt; <@U024H5LFB> : white wine : 9.75',
+    ('<@U02BMUNJ7> -&gt; <@U024H5LFB> white wine : 9.75',
         TestExpectation('U02BMUNJ7', 'U024H5LFB', 9.75, None, ' : white wine : '))
     ]
 
+#   r'@([0-9A-Z]+).?\s*([ -><]+)\s*@([0-9A-Z]+):?\s+\$?([0-9\.]+)\$?(.*)', text)
 
 def main():
-    for test in test_cases:
-        try:
-            run_test(test)
-        except AssertionError as err:
-            print(err, test, Transaction(test[0]))
+    for item, expectation in test_cases:
+        found = re.search(
+            r'<@(\w+)>.?\s*(\S*)\s*<@(\w+)>(?:[^0-9\.]+)?([0-9\.]+)', item)
+        # r'<@(\w+)>.?\s*(\S*)\s*<@(\w+)>\s+([^0-9\.]+)?([0-9\.]+)', item)
+            # r'@(\w+).?\s*(\S*)\s*(\w+)\s(.*?)([0-9\.]+)', item)
+        if found:
+            print(found.groups())
+        else:
+            print('nothing found for item: \n %s' % item)
+    #     try:
+    #         run_test(item, expectation)
+    #     except AssertionError as err:
+    #         print(err, item, Transaction(item))
+    #         raise
 
-    print("%d tests passed" % len(test_cases))
+    # print("%d tests passed" % len(test_cases))
 
 if __name__ == "__main__":
     main()
